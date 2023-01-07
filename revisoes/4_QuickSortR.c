e94ca6b25d45196f69caa3ce64cd3becf3a24074
Lucas <90059655+Lucas-ossli@users.noreply.github.com>
2023-01-07 20:22:35 -0300

    Quick Sort

---
+++ revisoes/4_QuickSortR.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int partition(int array[], int inicio, int fim);
+
+void quickSort(int array[], int inicio, int fim);
+
+int main(){
+	
+	
+	int i,size;
+	
+	printf("\nEnter the size of the array: ");
+	scanf("%d", &size);
+	int array[size];
+	
+	printf("\n\nArray 1st \n\n");
+	for(i=0 ; i < size ; i++){
+		array[i] = rand()%10000 - 1000;
+		printf("  %d", array[i]);
+		
+	}
+	
+	
+	
+	
+	quickSort(array,0,size);
+	
+	
+	
+	printf("\n\nArray after being sorted 2nd \n\n");
+	for(i=0 ; i < size ; i++){
+		printf("  %d", array[i]);
+	}
+	
+	printf("\n\n");
+	
+	return 0;
+}
+
+
+
+void quickSort(int array[], int inicio, int fim){
+	
+	int pivo;
+	
+	if(inicio < fim){
+		
+		pivo = partition(array,inicio,fim);
+		quickSort(array, inicio , pivo-1);
+		quickSort(array, pivo+1, fim);
+		
+	}
+	
+	return;
+	
+	
+}
+
+
+int partition(int array[], int inicio, int fim){
+	
+	
+	int pivo = array[fim];
+	int i = inicio;
+	int j,aux;
+	
+	for(j= inicio ; j < fim ; j++){
+		
+		if(array[j] <= pivo){
+			
+			aux = array[j];
+			array[j] = array[i];
+			array[i] = aux ;
+			i++;
+			
+		}
+		
+	}
+	
+	aux = array[i];
+	array[i] = array[fim];
+	array[fim] = aux;
+	
+	return i;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
