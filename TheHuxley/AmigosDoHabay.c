7c76fea4d26da5f02ed4510e4602253a11c2f115
Lucas <90059655+Lucas-ossli@users.noreply.github.com>
2023-01-17 21:16:55 -0300

    Tetando resolver um exercírcio TheHuxley

--- TheHuxley/AmigosDoHabay.c
+++ TheHuxley/AmigosDoHabay.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
 
 //https://www.thehuxley.com/problem/2834?locale=pt_BR
 
-//O critério para ser o escolhido é a quantidade de letras do primeiro nome, 
-//e em caso de empate, vence aquele que realizou primeiro a inscrição
 
-int maiorNome(char array[][50][100],int size);
-void selectionSort(char array[][50][100], int size);
-void converterParaArray(char array[][50][100], char *copia, int posicao);
+struct pessoa{
+	char nome[50];
+	char confirmacao[3];
+};
 
 
-//TO DO FAZER O CHAR ALUNO COMO UMA STRUCT
+int maiorNome(struct pessoa *aluno ,int size);
+void selectionSort(struct pessoa *aluno, int size);
+
 
 int main(){
 	
-	char aluno[2][50][100];
+	struct pessoa aluno[100];
+	//struct pessoa smaller[100];
+	struct pessoa amigoDoHabay;
+	//char aluno[2][50][100];
 	char fim[3] = {'F','I','M'};
+	
 	int i,j,x;
 	x=0;
 	
 	for(i=0 ; i<100 ; i++){
 		
-			//Nome
-			scanf("%s", &aluno[0][i]);	
-			//aux = aluno[0][i];
+			scanf("%s", &aluno[i].nome);
 			for(j=0 ; j<3 ; j++){
 				
-				if(fim[j] == aluno[0][i][j]){
+				if(fim[j] == aluno[i].nome[j]){
 					x++;
 				}
 				else{
@@ -41,25 +44,26 @@ int main(){
 				break;
 			}
 			else{
-				scanf("%s", &aluno[1][i]);	
+				scanf("%s", &aluno[i].confirmacao);	
+				
 			}
-		
+	
 	}
 	
-	char *amigoDoHabay;
-	amigoDoHabay= aluno[0][maiorNome(aluno,i)];
 	
+	struct pessoa amigoHabay ;
+	amigoHabay = aluno[maiorNome(&aluno,i)];
 	
-	selectionSort(aluno,i);
-	
+	selectionSort(&aluno,i);
 	
 	for(j=0 ; j<i ; j++){
 		
-		printf("\n\nnome:%s  Inscricao: %s ", aluno[0][j], aluno[1][j]);
-		
+		//if(aluno[j].confirmacao[0] == 'Y'){
+			printf("\nnome:%s ", aluno[j].nome);
+		//}
 	}
 	
-	printf("Amigo do Habay:\n%s", amigoDoHabay);
+	printf("\nAmigo do Habay:\n%s", amigoHabay.nome);
 	
 	return 0;
 }
@@ -67,28 +71,30 @@ int main(){
 
 
 
-int maiorNome(char array[][50][100],int size){
+int maiorNome(struct pessoa *aluno ,int size){
 	
 	int i,j,k,letras,aux,posicao;
 	j=0;
 	aux=0;
+	char yes[3] = {'Y','E','S'};
 	
 	for(i=0 ; i<size ; i++, j=0){
+		
 		letras = 0;
-		while(array[0][i][j] != '\0'){
+		while(aluno[i].nome[j] != '\0'){
 			letras++;
 			j++;
-			
 		}
 		
 		if(letras > aux ){
 			
-			for(k=0 ; array[1][i][k] != '\0' ; k++);
+			for(k=0 ; aluno[i].confirmacao[k] == yes[k] ; k++);
 			
 			if(k==3){
 				posicao = i;
 				aux=letras;
 			}
+			
 		}
 			
 		
@@ -98,49 +104,47 @@ int maiorNome(char array[][50][100],int size){
 	
 }
 
-
-
-
-//////////////////////////////////////////
-
-void selectionSort(char array[][50][100], int size){
+//TO DO - TENHO QUE FAZER UM OUTRO VETOR ORDENADO
+void selectionSort(struct pessoa *aluno, int size){
 	
-	int  i_smaller, place, i,j;
-	char *smaller;
-	char *aux;
+	int  i_smaller, place,i,j;
+	struct pessoa smaller;
 	place = 0;
 	
 	for(i=1 ; i < size ; i++){
 		
-		smaller =  array[0][place];
+		memcpy(&smaller, &aluno[place], sizeof(struct pessoa));
+		/
 		i_smaller = place;
 		
 		for(j=place+1 ; j <size ; j++){
 			
 			
-			if(array[0][j] < smaller ){
+			if(aluno[j].nome < smaller.nome){
 				
-				*smaller = array[0][j];
+				memcpy(&smaller, &aluno[j], sizeof(struct pessoa));
 				i_smaller = j;
 				
 			}
 			
 		}
-		   aux = array[0][place];
-		   *array[0][i_smaller] = *aux;
-		   *array[0][place] = *smaller; 	
 		
+		memcpy(&aluno[i_smaller], &aluno[place], sizeof(struct pessoa));
+			
+	    //aluno[i_smaller].nome = aluno[place].nome;
+	    //aluno[i_smaller].confirmacao = aluno[place].confirmacao;
+	   
+		memcpy(&aluno[place], &smaller, sizeof(struct pessoa));
+		// aluno[place].nome = nome;
+		// aluno[place].confirmacao = confirmacao;
+	   
+		   
 		place++;
 	}
 
 }
 
-//TO DO - fazer a copia completa de uma string para uma string simples
-void converterParaArray(char array[][50][100], char *copia, int posicao){
-	
-	
-	
-}
+
 
 
 
